9393fca6e3a387e896295362d3d93ce86e4908f5
brunoguirra <83722739+brunoguirra@users.noreply.github.com>
2021-05-05 16:08:26 -0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,91 @@
+<?php
+
+// ARQUIVO DE CONFIGURAÇÃO DO SITE
+// Para editar recomenda-se o uso de um editor de texto, ou de códigos, como o NotePad++, é fácil de usar, e você pode baixar ele gratuitamente, procure no google.
+
+// Para configurar o site, apenas edite o que estiver entre aspas, substitua os valores
+
+// ----------------- Dados do Site:
+
+// # Nome do site:
+$site_name="FEIRA IPTV";
+
+// # URL do site:
+$site_url="https://feiraiptv.xyz/"; // <<< não esqueça da / no final da URL! Se você instalar na raiz do site, use apenas https://seudominio.com.br/
+
+// # Email do Suporte:
+$email_suporte="contato.feiraiptv@gmail.com";
+
+// # Email do Suporte:
+$email_bank="contato.feiraiptv@gmail.com";
+
+// ----------------- Dados bancários do sistema:
+
+// Conta Bancária 1
+// Banco:
+$site_bank1="CEF";
+// Agencia:  
+$site_agencia1="3138 Op 013";
+// Nº da Conta:
+$site_nconta1="00058017-2";
+// Tipo - Poupança ou Corrente
+$site_tipo_conta1="poupança";
+
+// Conta Bancária 2
+// Banco:
+$site_bank2="BB";
+// Agencia:
+$site_agencia2="3128-3";
+// Nº da Conta:
+$site_nconta2="35745-6";
+// Tipo - Poupança ou Corrente
+$site_tipo_conta2="corrente";
+
+// Outros Métodos de Pagamento
+// Email MercadoPago:
+$site_neteller="emanuellemcarvalho@hotmail.com";
+// Chave PIX:
+$site_contasuper="05749499502";
+// Email Paypal:
+$site_paypal="breve";
+
+// --------------------- Configuração do Marketing do Sistema
+
+// Limite de ganhos no sistema, em relação ao valor investido, não inclui ganhos com formação de equipe, apenas ganhos com bônus divisão de lucro
+// valor em %, insira apenas números
+//$limite_ganhos="150";
+
+// limite de pacotes que um usuário pode comprar
+$packs_limite="3";
+
+// Valor mínimo para solicitar saque:
+$saque_minimo="50";
+// Valor da taxa de saque em % - será descontado do valor do saque, exemplo: o usuário tem R$500,00, solicitaa o saque, e recebrá R$450,00 se a taxa for de 10%
+$taxa_saque="3";
+
+// Quantidade mínima de indicados diretos ativos para ter direito ao bônus divisão de lucro, coloque zero se não deseja essa função
+$diretos_minimo_bdl="0";
+
+// Bônus liderança - Apartir de X vendas, ou indicados ativos pagantes,
+// o usuário passa a receber o dobro do Bônus Divisão de Lucro
+// Se não deseja oferecer este bônus, coloque o número 999
+$lider_minimo="999";
+
+// ABAIXO CONFIGURA AS BONIFICAÇÕES, PARA NÃO DAR BÔNUS EM UM NÍVEL, COLOQUE O NÚMERO zero
+// Comissão por downline direto = 1º nível / em %
+$bonus_nv1="20";
+
+// Comissão por downline indireto no 2º nível / em %
+$bonus_nv2="10";
+
+// Comissão por downline indireto no 3º nível / em %
+$bonus_nv3="5";
+
+// Comissão por downline indireto no 4º nível / em %
+$bonus_nv4="2";
+
+// Comissão por downline indireto no 5º nível / em %
+$bonus_nv5="1";
+
+
+?>
